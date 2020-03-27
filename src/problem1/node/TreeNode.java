fed24206ccec3d4549518900437233aa96b5336f
kishoritiwari2000 <kishoritiwari2000@gmail.com>
2020-03-27 02:52:10 -0700

    this is the required node class TreeNode for Problem 1

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,4 +7,35 @@
 package problem1.node;
 
 public class TreeNode {
+    private int value;
+    private TreeNode left,right;
+
+    public TreeNode(int item) {
+        value = item;
+        left = right = null;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
 }
